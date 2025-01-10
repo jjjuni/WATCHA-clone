e74e063f3dc7cbaa5b18809b6a502fc44060d697
jjjuni <0210leejun@gmail.com>
2025-01-10 17:38:07 +0900

    ✨ bottom bar routing

--- src/components/bar/bottombar.jsx
+++ src/components/bar/bottombar.jsx
@@ -1,14 +1,15 @@
 import { BiHomeAlt2, BiMoviePlay, BiSearch } from "react-icons/bi"
+import { Link } from "react-router-dom";
 import { BsPerson } from "react-icons/bs";
 
 const BottomBar = () => {
   
   return (
     <div className="w-[100vw] h-[45px] bg-[var(--main-gray)] fixed bottom-0 z-[100] flex justify-evenly sm:bottom-[-60px] transition-all ease-out duration-300">
-      <button className="w-[60px] text-white justify-items-center text-[20px]"><BiHomeAlt2/></button>
-      <button className="w-[60px] text-white justify-items-center text-[20px]"><BiMoviePlay/></button>
-      <button className="w-[60px] text-white justify-items-center text-[20px]"><BiSearch/></button>
-      <button className="w-[60px] text-white justify-items-center text-[20px]"><BsPerson/></button>
+      <Link to={'/'} className="w-[60px] text-white content-center justify-items-center text-[20px]"><BiHomeAlt2/></Link>
+      <Link to={'/movie-category'} className="w-[60px] text-white content-center justify-items-center text-[20px]"><BiMoviePlay/></Link>
+      <Link to={'/search'} className="w-[60px] text-white content-center justify-items-center text-[20px]"><BiSearch/></Link>
+      <Link to={'/'} className="w-[60px] text-white content-center justify-items-center text-[20px]"><BsPerson/></Link>
     </div>
   )
 }
